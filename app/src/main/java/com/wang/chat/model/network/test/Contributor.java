5dd1446b81b14617519f68b5c989e309fe2f60c3
wangyanxiang <wangyanxiang@tudou.com>
2016-07-14 10:15:07 +0800

    fix bugs of gson data

--- app/src/main/java/com/wang/chat/model/network/test/Contributor.java
+++ app/src/main/java/com/wang/chat/model/network/test/Contributor.java
@@ -1,9 +1,13 @@
 package com.wang.chat.model.network.test;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by wang on 16-7-4.
  */
 public class Contributor {
+    @SerializedName("login")
     public String login;
+    @SerializedName("contributions")
     public int contributions;
 }
